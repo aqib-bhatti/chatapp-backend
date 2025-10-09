69cce52c2a959279e07a970830c0f77bad21e657
Muhammad Aqib <saadibhatti1921@gmail.com>
2025-10-09 15:00:58 +0500

    Fix: use process.env.PORT for Railway deploy

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routers/authRoutes.js"
-import messageRoutes from './routers/messageRoutes.js'
-import groupRoutes from './routers/groupRoutes.js'
+import authRoutes from "./routers/authRoutes.js";
+import messageRoutes from "./routers/messageRoutes.js";
+import groupRoutes from "./routers/groupRoutes.js";
 import { connectDB } from "./lib/db.js";
-import cors from 'cors'
-import {app,server} from './lib/socket.js'
-dotenv.config()
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+dotenv.config();
 
-
-
-
-app.use(cors({
-  origin :process.env.FRONTEND_URI,
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URI,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-app.use("/api/groups",groupRoutes)
-const PORT = process.env.PORT || 2000;
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/groups", groupRoutes);
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
